1f102747dc98c13a1966bed993652c62e583c6f2
Julian Nadarzy <julian.nadarzy@gmail.com>
2021-10-22 10:12:56 +0200

    Initial commit

---
+++ src/test/java/com/nadarzy/betterReadsApp/betterReadsAppTests.java
@@ -0,0 +1,13 @@
+package com.nadarzy.betterReadsApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class betterReadsAppTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
